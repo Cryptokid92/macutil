576e88674afa81a4b06ad00f4f6a3a9027411bc7
Cryptokid92 <114345066+Cryptokid92@users.noreply.github.com>
2026-08-29 17:49:58 +0200

    feat(macutil): bind Install checkboxes to BrewClient
    MainWindow lists catalog apps as checkboxes. Detect uses BrewClient.installed.
    Install uses BrewClient.install with no sudo. Program.fs stays last.

--- MacUtilGUI/Views/MainWindow.axaml.fs
+++ MacUtilGUI/Views/MainWindow.axaml.fs
@@ -1,13 +1,10 @@
 namespace MacUtilGUI.Views
 
-open System.Windows.Input
 open Avalonia
 open Avalonia.Controls
 open Avalonia.Input
 open Avalonia.Markup.Xaml
 open Avalonia.Interactivity
-open MacUtilGUI.ViewModels
-open MacUtilGUI.Models
 
 type MainWindow() as this =
     inherit Window()
@@ -28,17 +25,6 @@ type MainWindow() as this =
                 WindowState.Normal
             else
                 WindowState.FullScreen
-
-    member private this.OnScriptButtonClick(sender: obj, e: RoutedEventArgs) =
-        match sender with
-        | :? Button as button ->
-            match button.Tag with
-            | :? ScriptInfo as script ->
-                match this.DataContext with
-                | :? MainWindowViewModel as vm -> (vm.SelectScriptCommand :> ICommand).Execute(script)
-                | _ -> ()
-            | _ -> ()
-        | _ -> ()
 
     member private this.TitleBar_PointerPressed(sender: obj, e: PointerPressedEventArgs) =
         this.BeginMoveDrag(e)
